2457894c0179bd22d4e9238809ddd6628b5936b8
Nhat Linh <nhatlinhdev201@gmail.com>
2025-02-22 15:03:29 +0700

    update/ key

--- src/Constants/Firebase/index.js
+++
@@ -1,5 +0,0 @@
-export default Firebase = {
-  API_KEY: "AIzaSyANLQ5mDcjbOoMJjmzZj7KZLrVhWIYpmhg",
-  DATABASE_URL:
-    "https://golden-bee-651eb-default-rtdb.asia-southeast1.firebasedatabase.app",
-};
